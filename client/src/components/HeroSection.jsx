2249a577edca076e4b050a75858549d27ef311ad
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-04-20 15:14:00 +0530

    f

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,9 +1,11 @@
 "use client";
 import React, { useState } from "react";
+import { FaSpinner } from "react-icons/fa";
 
 const Hero = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [response, setResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [missingValueStrategy, setMissingValueStrategy] = useState("ignore");
   const [outlierStrategy, setOutlierStrategy] = useState("ignore");
 
@@ -21,6 +23,7 @@ const Hero = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
     const formData = new FormData();
     formData.append("file", selectedFile);
@@ -35,8 +38,10 @@ const Hero = () => {
       );
       const data = await response.json();
       setResponse(data);
+      setIsLoading(false);
     } catch (error) {
       console.error("Error:", error);
+      setIsLoading(false);
     }
   };
 
@@ -73,7 +78,7 @@ const Hero = () => {
   return (
     <div className="h-screen py-10 md:py-20">
       <div className="container mx-auto text-center">
-        <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold mb-2 md:mb-4 lg:mb-6">
+        <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold my-16">
           Upload CSV File
         </h1>
         <input
@@ -130,39 +135,48 @@ const Hero = () => {
         <button
           onClick={handleClean}
           className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 transition duration-300 mt-4"
-          disabled={!selectedFile} // Disable button if no file is selected
+          disabled={!selectedFile}
         >
           Clean & Download
         </button>
       </div>
 
-      {response && (
-        <div className="mt-6 mx-8 md:mx-16">
-          <h2 className="text-center text-xl font-semibold mb-2">Summary</h2>
-          <pre className="whitespace-pre-wrap">
-            {response.success ? (
-              <>
-                <p className="font-semibold">Statistics:</p>
-                <ul>
-                  <li>Number of rows: {response.statistics.num_rows}</li>
-                  <li>Number of columns: {response.statistics.num_columns}</li>
-                  <li>
-                    Total missing values:{" "}
-                    {response.statistics.total_missing_values}
-                  </li>
-                  <li>
-                    Number of duplicates: {response.statistics.num_duplicates}
-                  </li>
-                  <li>
-                    Number of outliers: {response.statistics.num_outliers}
-                  </li>
-                </ul>
-              </>
-            ) : (
-              <p className="font-semibold">Error: {response.message}</p>
-            )}
-          </pre>
+      {isLoading ? (
+        <div className="mt-6 mx-8 md:mx-16 flex flex-col items-center">
+          <FaSpinner className="animate-spin text-4xl text-blue-500 mb-4" />
+          <p className="text-center text-xl font-semibold">Loading...</p>
         </div>
+      ) : (
+        response && (
+          <div className="mt-6 mx-8 md:mx-16">
+            <h2 className="text-center text-xl font-semibold mb-2">Summary</h2>
+            <pre className="whitespace-pre-wrap">
+              {response.success ? (
+                <>
+                  <p className="font-semibold">Statistics:</p>
+                  <ul>
+                    <li>Number of rows: {response.statistics.num_rows}</li>
+                    <li>
+                      Number of columns: {response.statistics.num_columns}
+                    </li>
+                    <li>
+                      Total missing values:{" "}
+                      {response.statistics.total_missing_values}
+                    </li>
+                    <li>
+                      Number of duplicates: {response.statistics.num_duplicates}
+                    </li>
+                    <li>
+                      Number of outliers: {response.statistics.num_outliers}
+                    </li>
+                  </ul>
+                </>
+              ) : (
+                <p className="font-semibold">Error: {response.message}</p>
+              )}
+            </pre>
+          </div>
+        )
       )}
     </div>
   );
