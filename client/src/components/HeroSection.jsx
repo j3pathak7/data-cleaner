f703ca75c50a69ae70d0ad5966b5478ec716662f
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-04-20 08:39:40 +0530

    f

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -96,7 +96,6 @@ const Hero = () => {
             <option value="interpolate">Interpolate</option>
           </select>
         </div>
-        <div>{missingValueStrategy}</div>
         <button
           onClick={handleSubmit}
           className="bg-yellow-500 m-4 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-yellow-600 transition duration-300"
